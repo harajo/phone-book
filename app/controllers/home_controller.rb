82e05a0e90a40216e4ff09e5e9a3f886b822a1ec
harajo <beautycho511@daum.net>
2015-01-09 08:56:00 +0000

    전화번호 저장 페이지 만들기

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,10 @@ class HomeController < ApplicationController
   end
 
   def add_process
+		n = Number.new
+		n.name = params[:phone_user]
+		n.phone_number = params[:phone_number]
+		n.save
+
   end
 end
